39f40487ab8c91d5a6f874d260b34c10eaf412be
BunnyBionics <morgan94122@gmail.com>
2024-11-06 17:27:35 -0800

    imported the rest of the HW files

--- src/Folder.java
+++ src/Folder.java
@@ -1,75 +1,39 @@
 import java.util.*;
 
-public class Folder  {
-	
-	private List<Folder> subFolders;
-	private List<FileItem> files;
-	
-	private String name;
+public class Folder {
 
+	private List<FileItem> fileList;
+	private List<Folder> subFolders;
+	private String name;
+	
 	public Folder(String name) {
 		this.name = name;
+		this.fileList = new ArrayList<FileItem>();
 		this.subFolders = new ArrayList<Folder>();
-		this.files = new ArrayList<FileItem>();
 	}
+	
 
-	public List<Folder> getSubFolders() {
-		return subFolders;
-	}
-	
-	public List<FileItem> getFiles() {
-		return files;
-	}
-	
 	public String getName() {
 		return name;
 	}
+	
+	public List<FileItem> getFileList() {
+		return fileList;
+	}
+	public List<Folder> getSubFolders() {
+		return subFolders;
+	}
 
+	public boolean addFile(FileItem file) {
+		return fileList.add(file);
+	}
+	
+	public boolean addSubFolder(Folder folder) {
+		return subFolders.add(folder);
+	}
 
 	@Override
 	public String toString() {
 		return name;
 	}
-
-	public void addFile(FileItem file) {
-		files.add(file);
-	}
-	
-	public void addSubFolder(Folder folder) {
-		subFolders.add(folder);
-	}
-	
-	public int getSizeOfFiles() {
-		int fileSize = 0;
-		for(FileItem file : files) {
-			fileSize += file.getSize();
-		}
-		return fileSize;
-	}
-	
-	public int getSize() {
-		int totalSize = getSizeOfFiles();
-		for(Folder folder : subFolders) {
-			totalSize += folder.getSize();
-		}
-		return totalSize;
-	}
-
-	public boolean containsFileDirectly(FileItem file) {
-		return files.contains(file);
-	}
-	
-	public boolean containsFile(FileItem file) {
-		if(containsFileDirectly(file)) {
-			return true;
-		} else {
-			for(Folder folder : subFolders) {
-				if(folder.containsFile(file)) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
-	
 }
